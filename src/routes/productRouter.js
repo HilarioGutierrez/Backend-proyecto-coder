685ee1b69849093a2ad8ad404d4f55656db28a5e
Hilario Gutierrez <hilario.m.gutierrez@gmail.com>
2023-03-29 20:20:08 -0300

    feat: Router Express

--- src/routes/productRouter.js
+++ src/routes/productRouter.js
@@ -19,18 +19,18 @@ productRouter.get('/', async (req, res) => {
 });
 
 //Endpoint. URL Req.params to get the id of the product to show
-// productRouter.get('/products/:pid', async (req, res) => {
-//     const products = await manager.getProducts();
-//     const param = +req.params.pid;
-//     //filtered id's products same as param.
-//     const filteredProducts = products.filter(p => p.id === param);
+productRouter.get('/products/:pid', async (req, res) => {
+    const products = await manager.getProducts();
+    const param = +req.params.pid;
+    //filtered id's products same as param.
+    const filteredProducts = products.filter(p => p.id === param);
 
-//     //if FilteredProducts is empty, send error message. Else send the product
-// if(filteredProducts.length === 0) {
-//         res.status(404).send({error: 'Sorry. Product not found'});
-//         return;
-// }
-//     res.send(filteredProducts);
-// });
+    //if FilteredProducts is empty, send error message. Else send the product
+if(filteredProducts.length === 0) {
+        res.status(404).send({error: 'Sorry. Product not found'});
+        return;
+}
+    res.send(filteredProducts);
+});
 
 export default productRouter;
