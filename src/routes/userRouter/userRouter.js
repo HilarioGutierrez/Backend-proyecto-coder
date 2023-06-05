63c220a57411b029438818ab39a4bf901d370164
Hilario Gutierrez <hilario.m.gutierrez@gmail.com>
2023-06-05 18:35:51 -0300

    add authorization on user

--- src/routes/userRouter/userRouter.js
+++ src/routes/userRouter/userRouter.js
@@ -1,17 +1,18 @@
 import { Router } from "express";
 import {  create, deleteOne, getOne, list, updateOne } from "../../controllers/userController.js";
 import auth from "../../middlewares/auth.js";
+import authorization from "../../middlewares/authorization.js";
 
 const userRouter = new Router();
 
-userRouter.get('/:email', getOne);
+userRouter.get('/:email', auth, authorization('getUser'), getOne);
 
-userRouter.get('/', list);
+userRouter.get('/', auth, authorization('getUsers'), list);
 
-userRouter.post('/', auth, create);
+userRouter.post('/', auth,authorization('createUser'), create);
 
-userRouter.put('/:email', updateOne);
+userRouter.put('/:email', auth, authorization('updateUser'), updateOne);
 
-userRouter.delete('/:email', deleteOne);
+userRouter.delete('/:email', auth, authorization('deleteUser'), deleteOne);
 
 export default userRouter
