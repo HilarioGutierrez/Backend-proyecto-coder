304996a2db362bb983a051d3dd4e830c231e22ae
Hilario Gutierrez <hilario.m.gutierrez@gmail.com>
2023-06-05 18:35:18 -0300

    add authorization on roles

--- src/routes/roleRouter/roleRouter.js
+++ src/routes/roleRouter/roleRouter.js
@@ -1,17 +1,18 @@
 import { Router } from "express";
 import {  create, deleteOne, getOne, list, updateOne } from "../../controllers/roleController.js";
 import auth from "../../middlewares/auth.js";
+import authorization from "../../middlewares/authorization.js";
 
 const roleRouter = new Router();
 
-roleRouter.get('/:id', auth, getOne);
+roleRouter.get('/', auth, authorization('getRoles'), list);
 
-roleRouter.get('/', auth, list);
+roleRouter.get('/:id', auth, authorization('getRole'), getOne);
 
-roleRouter.post('/', auth, create);
+roleRouter.post('/', auth, authorization('getRole'), create);
 
-roleRouter.put('/:id', auth, updateOne);
+roleRouter.put('/:id', auth, authorization('getRole'), updateOne);
 
-roleRouter.delete('/:id', auth, deleteOne);
+roleRouter.delete('/:id', auth, authorization('getRole'), deleteOne);
 
 export default roleRouter
