a3552f243a0357f65888ce3097d877f637352e27
Hilario Gutierrez <hilario.m.gutierrez@gmail.com>
2023-06-22 01:07:47 -0300

    factories: db

--- src/command.js
+++ src/command.js
@@ -4,18 +4,14 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-import mongoose from 'mongoose';
-
 import addUser from './shared/commands/addUser.js';
+import dbFactory from './data/factories/dbFactory.js';
 
 void(async () =>
 {
     try {
-        await mongoose.connect(process.env.MONGO_DB_URI, 
-            {
-                useNewUrlParser: true,
-                useUnifiedTopology: true
-            });
+        const db = dbFactory.create(process.env.DB);
+        db.connect(process.env.MONGO_DB_URI);
 
             program.addCommand(addUser);
 
