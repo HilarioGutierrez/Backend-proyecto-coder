768f24c52e64bac6fb4d597a37d1ee571d3e1f90
Hilario Gutierrez <hilario.m.gutierrez@gmail.com>
2023-08-25 01:18:56 -0300

    test corrections

---
+++ src/domain/utils/generateUser.js
@@ -0,0 +1,17 @@
+import { faker } from "@faker-js/faker";
+import User from "../entities/user.js";
+
+export const generateUser = () => {
+    const user = new User({
+        firstName: faker.person.firstName(),
+        lastName: faker.person.lastName(),
+        email: faker.internet.email(),
+        age: 25 ,
+        isAdmin: false,
+        cart: null,
+        permissions: [],
+        password: faker.internet.password()
+    });
+
+    return user;
+};
